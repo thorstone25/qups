6ac9b0413f2df5af40a77621c6fcfaf9808585cb
Thurston Brevett <tbrevett@stanford.edu>
2022-04-25 23:41:58 -0700

    Removed R2020b features.

--- utils/sub.m
+++ utils/sub.m
@@ -6,30 +6,16 @@
 % y = SUB(x, ind, dim) slices x in dimension dim instead of dimension 1
 %
 % y = SUB(x, ind, vecdim) slices x with the same indices in each dimension 
-% in vecdim i.e. returns x(:,...,:,ind,:,...,:,ind,:,...,:)
-%
+% in vecdim i.e. returns x(:,...,:,ind,:,...,:,ind,:,...,:). This is not
+% very useful, and will probably be deprecated soon. 
+% 
 % Example:
 % x = rand(2,3,4);
 % all(x(:,2:3,:) == sub(x,2:3,2), 'all') 
 % 
-% 
+
 function y = sub(x, ind, dim)
-arguments
-    x
-    ind (1,:) {mustBeIndex}
-    dim (1,:) double {mustBeInteger, mustBePositive} = 1
-end
+if nargin < 3, dim = 1; end
 subs = cellstr(repmat(":", [1, max(ndims(x), max(dim))]));
 [subs{dim}] = deal(ind);
 y = subsref(x, substruct('()', subs));
-
-
-function mustBeIndex(ind)
-arguments
-    ind (1,:) {mustBeInteger, mustBeNonnegative}
-end
-
-if isnumeric(ind)
-    mustBePositive(ind);
-end
-    
