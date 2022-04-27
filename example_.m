aea08958f12cbfc7c85d9c727a553121ea2707ad
Thurston Brevett <tbrevett@stanford.edu>
2022-04-26 21:06:01 -0700

    Updated example files to include Apodization.

--- example_.m
+++ example_.m
@@ -3,7 +3,7 @@
 % QUPS is designed to be an accessible, verastile, shareable, lightweight codebase 
 % designed to make developing and running ultrasound algorithms quick and easy! 
 % It offers a standardization of data formatting that serves most common pulse-echo 
-% ultrasound systems and supports simulation programs including k-Wave and Fullwave.
+% ultrasound systems and supports simulation programs such as k-Wave or Fullwave.
 %% Why?
 % This package seeks to lower the barrier to entry for doing research in ultrasound 
 % by offering a lightweight, easy to use package. Most of the underlying implementations 
@@ -14,24 +14,25 @@
 % * Create linear and curvilinear (convex) transducers
 % * Define full-synthetic-aperture, focused, diverging, or plane-wave transmits
 % * Beamform to create a b-mode image
-% * Simulate point targets or distributed media via k-Wave or Fullwave
+% * Simulate point targets or distributed media via k-Wave
 %% 
 % 
 % Setup the workspace
 
 %#ok<*UNRCH> ignore unreachable code due to constant values
 %#ok<*BDLGI> ignroe casting numbers to logical values
-device = -1; % select 0 for cpu, -1 for gpu if you have one
-if device, setup;  % add all the necessary paths
-else, setup parallel; % also starts a parpool for faster processing
-end
+device = -logical(gpuDeviceCount); % select 0 for cpu, -1 for gpu if you have one
+% setup;  % add all the necessary paths
+setup parallel; % also starts a parpool for faster processing
+
+try hw = waitbar(0, 'test', 'Visible', 'off'); close(hw); end %#ok<TRYNC> % R2021b bug - errors on first call
 %% 
 %% Create a simple simulation
 % Choose a Target
 
-target_depth = 30;
+target_depth = 1e-3 * 30;
 switch "single"
-    case 'single', targ = Target('pos', [0;0;target_depth*1e-3], 'c0', 1500); % simple point target
+    case 'single', targ = Target('pos', [0;0;target_depth], 'c0', 1500); % simple point target
     case 'array' , targ = Target('pos', [0;0;1] * 1e-3*(10:5:50)); % targets every 5mm
 end
 targ.rho_scat = 2; % make density scatterers at 2x the density
@@ -54,13 +55,14 @@ end
 % For linear transducers only!
 
 if isa(xdc, 'TransducerArray') 
-    switch "Plane-wave"
+    switch "Focused"
         case 'FSA', seq = Sequence('type', 'FSA', 'c0', targ.c0, 'numPulse', xdc.numel); % set a Full Synthetic Aperture (FSA) sequence
         case 'Plane-wave', seq = SequenceRadial('type', 'PW', ...
-                'ranges', 1, 'angles', -30:10:30, 'c0', targ.c0); % Plane Wave (PW) sequence
+                'ranges', 1, 'angles', -10:2:10, 'c0', targ.c0); % Plane Wave (PW) sequence
         case 'Focused', seq = Sequence('type', 'VS', 'c0', targ.c0, ...
-        'focus', [1;0;0] .* 1e-3*(-10 : 2 : 10) + [0;0;30e-3] ...
-        ); % translating aperture: depth of 30mm, lateral stride of 2mm
+        'focus', [1;0;0] .* 1e-3*(-10 : 2.0 : 10) + [0;0;target_depth] ... % translating aperture: depth of 30mm, lateral stride of 2mm
+        ... 'focus', [1;0;0] .* 1e-3*(-10 : 0.2 : 10) + [0;0;target_depth] ... % translating aperture: depth of 30mm, lateral stride of 0.2 mm
+        ); 
     end
 
     
@@ -68,9 +70,10 @@ if isa(xdc, 'TransducerArray')
 % For convex transducers only!
 
 elseif isa(xdc, 'TransducerConvex') 
-    switch "FSA"
+    switch "sector"
         case 'sector'
-            seq = SequenceRadial('type', 'VS', 'c0', targ.c0, 'angles', -45:5:45, ...
+            seq = SequenceRadial('type', 'VS', 'c0', targ.c0, ...
+                'angles', -5:1:5, ...
                 'ranges', norm(xdc.center) + 35e-3, 'apex', xdc.center ...
                 ); % sector scan sequence
         case 'FSA'
@@ -106,10 +109,13 @@ elseif isa(xdc, 'TransducerConvex')
 end
 %% 
 % 
+% 
+% Show the transducer's impulse response
 
-% show the transducer's impulse response
 figure; plot(xdc.impulse, '.-'); title('Element Impulse Response'); 
-%  Plot configuration of the simulation
+%% 
+% Plot configuration of the simulation
+
 figure; hold on; title('Geometry');
 imagesc(targ, tscan); colorbar; % show the background medium for the simulation/imaging region
 plot(xdc, 'r+', 'DisplayName', 'Elements'); % elements
@@ -129,8 +135,9 @@ us = UltrasoundSystem('xdc', xdc, 'sequence', seq, 'scan', scan, 'fs', 40e6);
 % Simulate a point target
 % run on CPU to use spline interpolation
 % chd0 = calc_scat_all(us, targ, [1,1], 'device', 0, 'interp', 'spline'); % use FieldII, 
-chd0 = comp_RS_FSA(us, targ, [1,1], 'method', 'interpn', 'device', 0, 'interp', 'spline'); % use a Greens function
-chd0 %#ok<NOPTS> % show the output
+% chd0 = comp_RS_FSA(us, targ, [1,1], 'method', 'interpn', 'device', 0, 'interp', 'spline'); % use a Greens function
+chd0 = comp_RS_FSA(us, targ, [1,1], 'method', 'interpn', 'device', device, 'interp', 'linear'); % use a gpu if available!
+chd0
 
 % display the channel data across the transmits
 chd = mod2db(chd0); % == 20*log10(abs(x)) -> the power of the signl in dB
@@ -145,9 +152,68 @@ chd = single(chd); % use less data
 chd.data = chd.data - mean(chd.data, 1, 'omitnan'); % remove DC 
 if device, chd = gpuArray(chd); end % move data to GPU
 if isreal(chd.data), chd = hilbert(chd, 2^nextpow2(chd.T)); end % apply hilbert on real data
+chd = rectifyt0(chd, 'linear'); % linearly resample if needed to align t0 time axis
 
 % Run a simple DAS algorithm
-b = DAS(us, chd, struct('c0', targ.c0), [], 'device', device, 'interp', 'linear');
+switch seq.type
+    case "VS", 
+        switch "accept"
+            case 'multiline', apod = multilineApodization(us.scan, us.sequence);
+            case 'scanline', apod = scanlineApodization(us.scan, us.sequence);
+            case 'translating', apod = translatingApertureApodization(us.scan, us.sequence, us.rx, 7.2*1e-3);
+            case 'aperture-growth', apod = apertureGrowthApodization(us.scan, us.sequence, us.rx, 1.5, us.rx.aperture_size);
+            case 'accept', apod = acceptanceAngleApodization(us.scan, us.sequence, us.rx, 45); 
+            case 'none', apod = 1;
+        end        
+    otherwise, apod = 1;
+end
+b = DAS(us, chd, struct('c0', targ.c0), [], 'device', device, 'interp', 'linear', 'apod', apod);
+
+% show the image
+b_im = mod2db(b); % convert to power in dB
+figure; imagesc(scan, b_im, [-120, 0] + max(b_im(:))); % display with 60dB dynamic range
+colormap gray; colorbar;
+%% Scan Convert for a Sector Scan
+
+if ~isa(scan, 'ScanCartesian')
+    scanc = scanCartesian(scan); % mind the caps! this gives us a ScanCartesian
+    [scanc.nx, scanc.nz] = deal(2^9); % don't change boundaries, but increase resolution
+    bc_im = (scanConvert(scan, mod2db(b), scanc)); % interpolate in dB - could also do abs, as long as it's not complex!
+
+    imagesc(scanc, bc_im, [-60, 0] + max(bc_im(bc_im < Inf)));
+    colormap gray; colorbar;
+end
+%% Run a k-Wave Sim (requires k-Wave)
+% Note: For a FSA acquisition, that means 1 sim per element!
+
+% use some low-res simulation parameters 
+% Takes about 1 min per sim, 1 sim per tx
+bds = cat(1,tscan.xb, tscan.yb, tscan.zb);
+kwv_args = {'dims', 2, 'CFL_max', 0.5, 'resolution_ratio', 0.25, 'PML_min', 2^4, 'PML_max', 2^6, ... 
+    'bounds', bds, 'PlotSim', false ...
+    };
+if device, chd1 = kspaceFirstOrderND(us, targ, [1,1], kwv_args{:}); % use just a single sub-element for speed
+else,      chd1 = kspaceFirstOrderND(us, targ, [1,1], kwv_args{:}, 'DataCast', 'single'); end
+% Show the data
+
+chd = copy(chd1);
+% chd.data(chd.time < 20e-6,:,:) = nan; % normalize display to data after 20us 
+figure; h = imagesc(chd, 1); colormap jet; colorbar; caxis([-80 0] + mod2db(max(chd.data(:), [], 'omitnan')))
+xlabel('Channel'); ylabel('Time (s)');
+for m = 1:size(chd.data,3), if isvalid(h), h.CData(:) = mod2db(chd.data(:,:,m)); drawnow limitrate; title(h.Parent, "Tx " + m); pause(1/10); end, end
+%% Create an image
+
+% Precondition the data
+chd = copy(chd1);
+chd = single(chd); % use less data
+chd.data(chd.time < 10e-6 | chd.time > 50e-6,:,:) = 0; % clear direct feedback
+chd.data = chd.data - mean(chd.data, 1, 'omitnan'); % remove DC bias
+if device, chd = gpuArray(chd); end % move data to GPU
+if isreal(chd.data), chd = hilbert(chd, 2^nextpow2(chd.T)); end % apply hilbert on real data
+chd = rectifyt0(chd, 'linear'); % linearly resample if needed to align t0 time axis
+
+% Run a simple DAS algorithm
+b = DAS(us, chd, struct('c0', targ.c0), [], 'device', -1, 'interp', 'linear');
 
 % show the image
 b_im = mod2db(b); % convert to power in dB
@@ -160,38 +226,6 @@ if ~isa(scan, 'ScanCartesian')
     [scanc.nx, scanc.nz] = deal(2^9); % don't change boundaries, but increase resolution
     bc_im = (scanConvert(scan, mod2db(b), scanc)); % interpolate in dB - could also do abs, as long as it's not complex!
 
-    imagesc(scanc, bc_im, [-60, 0] + max(bc_im(:)));
+    imagesc(scanc, bc_im, [-60, 0] + max(bc_im(bc_im < Inf)));
     colormap gray; colorbar;
 end
-%% Run a k-Wave Sim (requires k-Wave)
-% Note: For a FSA acquisition, that means 1 sim per element!
-
-% use some low-res simulation parameters - tis may give some numerical
-% dispersion effects. Takes about 1 min per sim, 1 sim per tx
-kwv_args = {'dims', 2, 'CFL_max', 0.25, 'resolution_ratio', 0.25, 'PML_min', 1, 'PML_max', 2^6};
-if device, chd1 = kspaceFirstOrderND(us, targ, [1,1], kwv_args{:}); % use just a single sub-element for speed
-else, chd1 = kspaceFirstOrderND(us, targ, [1,1], kwv_args{:}, 'DataCast', 'single'); end
-% Show the data
-
-chd = chd1;
-figure; h = imagesc(chd, 1); colormap jet; colorbar; caxis([-80 0] + mod2db(max(chd.data(:), [], 'omitnan')))
-xlabel('Channel'); ylabel('Time (s)');
-for m = 1:size(chd.data,3), if isvalid(h), h.CData(:) = mod2db(chd.data(:,:,m)); drawnow limitrate; title(h.Parent, "Tx " + m); pause(1/10); end, end
-%% Create an image
-
-% Precondition the data
-chd = copy(chd1);
-chd = single(chd); % use less data
-chd.data(chd.time < 20e-6,:,:) = 0; % clear direct feedback
-% chd.t0 = chd.t0 + 4.5e-6;
-% chd.data = chd.data - mean(chd.data, 1, 'omitnan'); % remove DC 
-if device, chd = gpuArray(chd); end % move data to GPU
-if isreal(chd.data), chd = hilbert(chd, 2^nextpow2(chd.T)); end % apply hilbert on real data
-
-% Run a simple DAS algorithm
-b = DAS(us, chd, struct('c0', targ.c0), [], 'device', -1, 'interp', 'linear');
-
-% show the image
-b_im = mod2db(b); % convert to power in dB
-figure; imagesc(scan, b_im, [-60, 0] + max(b_im(:))); % display with 60dB dynamic range
-colormap gray; colorbar;
