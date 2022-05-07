989072c0f5569a73f6d1060f0661b0d11716c7d1
Thurston Brevett <tbrevett@stanford.edu>
2022-05-07 01:54:20 -0700

    Updated help.

--- utils/swapdim.m
+++ utils/swapdim.m
@@ -1,4 +1,14 @@
 function [x, ord] = swapdim(x, i, o)
+% SWAPDIM Swap dimensions
+%
+% y = SWAPDIM(x, i, o) swaps dimensions i and o in the array x. 
+%
+% Example:
+%   x = rand([1,2,3,4,5]);
+%   y = swapdim(x,2,4);
+%   assert(all(size(y) == [1,4,3,2,5]))
+
+assert(length(i) == length(o), 'Dimensions to swap must be the same length.');
 ord = 1:max([ndims(x), i, o]);
 ord(i) = o;
 ord(o) = i;
