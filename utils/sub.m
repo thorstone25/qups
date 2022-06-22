69a2f7719256b1415182e2a531a1574a4208ed21
Thurston <tbrevett@gmail.com>
2022-06-22 10:30:48 -0700

    sub utility returns indexing expression.

--- utils/sub.m
+++ utils/sub.m
@@ -7,18 +7,22 @@
 %
 % y = SUB(x, vecind, vecdim) slices x indices in each specified dimension 
 % in vecdim i.e. returns x(:,...,:,ind,:,...,:,ind,:,...,:). vecind must be
-% a cell array of indices. 
+% a cell array of indices.
+%
+% i = SUB(..., true) returns the indexing expression used to index the
+% object. This is useful to write statements using subsasgn and subsref.
 % 
 % Example:
 % x = rand(2,3,4,5,4,3,2);
 % assert(isequal(x(2,:,:,:,:,:,:), sub(x,2,1))); 
 % assert(isequal(x(:,2:3,4,:,:,:,:), sub(x,{2:3,4},[2 3])))
 %
-% 
+% See also SUBSREF SUBSASGN SUBSTRUCT
 
-function y = sub(x, ind, dim)
+function y = sub(x, ind, dim, expr)
 % default to dim 1. TODO: default to 1st non-singleton
-if nargin < 3, dim = 1; end 
+if nargin < 3, dim = 1; end
+if nargin < 4, expr = false; end
 
 % ensure indices placed in a cell
 if ~iscell(ind), ind = {ind}; end 
@@ -32,5 +36,8 @@ subs = cellstr(repmat(":", [1, max(gather(ndims(x)), max(dim))]));
 % replace ':' with index in selected dimensions
 for i = 1:numel(dim), subs{dim(i)} = ind{i}; end
 
-% index the object
-y = subsref(x, substruct('()', subs));
+% get object indexing expression
+iy = substruct('()', subs);
+
+% return the indexed object, or the expression
+if expr, y = iy; else, y = subsref(x, iy); end
