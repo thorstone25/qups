e974ad54829b7ee1b345370f51fa6936c7d72968
Thurston Brevett <tbrevett@stanford.edu>
2022-07-07 00:35:07 -0700

    slsc() changes: fixed average estimator weights; can compute for selected lags; added argument validation, problem rescaling, time sample dimension.

--- kern/slsc.m
+++ kern/slsc.m
@@ -1,15 +1,34 @@
-function z = slsc(x, dim, L, method, varargin)
+function z = slsc(x, dim, L, method, kdim)
 % SLSC - Short-lag Spatial Coherence 
 %
 % z = SLSC(x) computes the short-lag spatial coherence (SLSC) across the
 % data x. x can be any ND-array.
 %
-% z = SLSC(x, dim) operates in dimension dim. The default is 2.
+% z = SLSC(x, dim) operates in dimension dim. The default is the first 
+% non-singleton dimension.
 % 
-% z = SLSC(x, dim, L) uses up to L lags.
+% z = SLSC(x, dim, L) uses up to L lags. If L is an array of indices, the
+% indices matching L are used. Select a single lag by making an array
+% including 0 e.g. L = [0 5]. The default is 
+% max(1, floor(1/4 * size(x,dim)));
 % 
 % z = SLSC(x, dim, L, method) uses the specified method. Must be one of
 % {"ensemble"* | "average"}.
+% 
+% z = SLSC(x, dim, L, method, kdim) designates a temporal sample dimension
+% used in calculating the average SLSC. The default is ndims(x) + 1.
+% 
+% Example:
+% 
+% % generate a sine wave with random phase error
+% T = 2^4; % number of samples
+% fc = 2; % normalized frequency
+% t = (0:T-1)'/T; % time vector
+% phi = pi/2*rand([1,64]); % random phase differences
+% x = exp(1j*(2*pi*fc*t + phi)); % sine wave
+%
+% % get the SLSC across the phases
+% z = slsc(x, 2, [0 3], 'average')
 % 
 % References:
 % D. Hyun, A. C. Crowley and J. J. Dahl, 
@@ -25,83 +44,72 @@ function z = slsc(x, dim, L, method, varargin)
 % doi: 10.1109/TUFFC.2011.1957
 
 % defaults
-if nargin < 2 || isempty(dim   ), dim    = 2;          end
-if nargin < 3 || isempty(L     ), L      = 10;         end
-if nargin < 4 || isempty(method), method = "average"; end
+arguments
+    x
+    dim (1,1) {mustBePositive, mustBeInteger} = prod(find(size(x,1:ndims(x)) ~= 1, 1, 'first')); % lag dimension
+    L (1,:) {mustBeNonnegative, mustBeInteger} = max(1, floor(size(x,dim)/4)); % lags or maximum lag
+    method (1,1) string {mustBeMember(method, ["average", "ensemble"])} = "average"; % average or ensemble estimator
+    kdim (1,1) {mustBePositive, mustBeInteger} = ndims(x) + 1; % time sample dimension
+end
 
 % default parpool: this is data heavy, so only use a threadpool
-if isa(gcp('nocreate'), 'parallel.threadPool'), clu = gcp(); else, clu = 0; end
-if isa(x, 'gpuArray'), clu = 0; end % don't use any pool for gpu data
-kwargs.parcluster = clu;
-
-% assign optional inputs
-for i = 1:2:numel(varargin), kwargs.(varargin{i}) = varargin{i+1}; end
-
-% parse inputs
-clu = kwargs.parcluster;
+% don't use any pool for gpu data
+if isa(gcp('nocreate'), 'parallel.threadPool') ...
+        && ~isa(x, 'gpuArray') && ~isa(x, 'tall'), 
+    clu = gcp(); else, clu = 0; 
+end
 
 % get weighting filter across receiver pairs
-K = gather(size(x,dim)); % the full size of the aperture
-[M, N] = ndgrid(1:K, 1:K);
+A = gather(size(x,dim)); % the full size of the aperture
+[M, N] = ndgrid(1:A, 1:A);
 H = abs(M - N); % lags for each receiver/cross-receiver pair
-S = (0 < H & H <= L); % valid lags for adding (i.e. the short lags)
+if isscalar(L), lags = 1:L; else, lags = L; end % chosen lags for adding (i.e. the short lags)
+S = ismember(H,lags); % selection mask for the lags
+L = length(lags); % number of lags - this normalizes the sum of the average estimates
+K = gather(size(x,kdim)); % number of samples in time
 
 % choose average or ensemble
 switch method
     case "average"
         % normalize magnitude per sample (averaging)
-        x = x ./ abs(x);
+        x = x ./ vecnorm(x,2,kdim) / K;
         x(isnan(x)) = 0; % 0/0 -> 0
-        % TODO: test if abs(x) close to zero instead of assuming all nans
+        % TODO: test if norm(x) close to zero instead of assuming all nans
         % are from computing 0/0 
 
-        % get weighting filter across receiver pairs
-        W = S ./ (K - H); % final weights per pair (debiased)
-
-        % place cross-receiver across cells
-        % xc = num2cell(x, setdiff(1:ndims(x), dim));
+        % get weighting / filter across receiver pairs
+        W = S ./ (A - H) / 2 / L; % weights per pair (debiased, pos & neg averaged, multi-correlation-averaged)
 
         % place weights as cross receiver over cells, receiver in dim
         W = num2cell(shiftdim(W, -(dim-2)), dim);
 
-        % correlation sum across receivers per cross-receiver kernel
-        %vn = @(w,xc) sum(w .* conj(xc) .* x, dim);
-
         % compute product and sum per cross-receiver
         z = 0;
-        parfor (i = 1:numel(W), clu)
-            % z = z + vn(W{i}, xc{i});
-            z = z + sum(W{i} .* conj(sub(x,i,dim)) .* x, dim);
+        parfor (i = 1:A, clu)
+            z = z + sum(W{i} .* conj(sub(x,i,dim)) .* x, [dim, kdim], 'omitnan');
         end
 
     case "ensemble"
-        % place cross-receiver across cells
-        % xc = num2cell(x, setdiff(1:ndims(x), dim));
-        for i = gather(size(x,dim)):-1:1, xc{i,1} = sub(x,i,dim); end
-        xc = shiftdim(xc, 1-dim);
+        % scale the problem to maintain numerical accuracy
+        % we do this as precisely as possible by shifting by a power of 2.
+        x = x .* 2.^nextpow2(1/mean(vecnorm(x,2,dim),"all",'omitnan'));
 
         % place weights as cross receiver over cells, receiver in "dim"
         W = num2cell(shiftdim(S, -(dim-2)), dim);
 
-        % correlation across receivers per cross-receiver kernel
-        vn = @(w,xc) deal(...
-            sum(w .* conj(xc) .* x , dim),...
-            sum(w .* conj(x ) .* x , dim),...
-            sum(w .* conj(xc) .* xc, dim) ...
-            );
-
         % compute and sum for each cross-receiver
         [z, a, b] = deal(0);
-        parfor (i = 1:numel(W), clu)
-            [zr, ar, br] = vn(W{i}, xc{i});
-            z = z + zr;
-            a = a + ar;
-            b = b + br;
+        %parfor (i = 1:numel(W), clu)
+        for i = 1:A
+            w = W{i}; % weight for this set of indices
+            xc = sub(x,i,dim); % cross-receiver
+            z = z + sum(w .* conj(xc) .* x , [dim, kdim],'omitnan'); % inner-product
+            a = a + sum(w .* conj(x ) .* x , [dim, kdim],'omitnan'); % a-norm
+            b = b + sum(w .* conj(xc) .* xc, [dim, kdim],'omitnan'); % b-norm
         end
 
-        % get final image
-        z = z ./ sqrt(a .* b);
-    otherwise, error("Unrecognized method '" + method + "'");
+        % normalize by the norm of the vector
+        z = z .* rsqrt(a) .* rsqrt(b);
 end
 end
 
