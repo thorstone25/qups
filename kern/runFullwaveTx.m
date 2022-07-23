98afd11859d9089de14ee21a57072bd363dfdf39
Thurston Brevett <tbrevett@stanford.edu>
2022-07-23 12:20:44 -0700

    runFullwaveTx() accepts relative paths.

--- kern/runFullwaveTx.m
+++ kern/runFullwaveTx.m
@@ -21,42 +21,51 @@ if nargin < 2, simdir = pwd; end
 if nargin < 3, outdir = tempname(simdir); end
 ogdir = pwd;
 
+% get full path of simulation directory
+simdir = getfield(what(simdir), 'path');
+
 % move to (temporary) simulation directory on the local machine
 tmp = tempname(); 
 mkdir(tmp);
-cd(tmp);
-
-disp("Running from " + pwd());
 
 try % protect against not cleaning-up errors
     % ensure that the output directory is in fact a directory
-    mkdir(fullfile(outdir));
+    copyfile(fullfile(simdir, 'fullwave2_executable'), tmp); % copy over the executable
 
-    % write the data to file in the tmp directory
-    fid = fopen('icmat.dat','w+');
-    fwrite(fid, real(icmat), 'float');
-    fclose(fid);
+    % make the output directory
+    mkdir(fullfile(outdir));
 
     % link to all input (.dat) files
     listing = dir(simdir);
     dfiles = string({listing.name});
     dfiles = dfiles(endsWith(dfiles, '.dat')); % all .dat files
     dfilenames = fullfile(simdir, dfiles); % full paths
-    err = system(join("ln -s " + dfilenames + " . ;")); % symbolic link
+    err = system(join("ln -s " + dfilenames + " " + tmp + " ; ")); % symbolic link
     if err % link didn't work: copy everything?
         warning('Unable to link files: copying instead.');
         arrayfun(@copyfile, dfilenames); % copy everything
     end
 
+    % move to the tmp directory
+    cd(tmp);
+
+    % write the data to file in the tmp directory
+    fid = fopen('icmat.dat','w+');
+    fwrite(fid, real(icmat), 'float');
+    fclose(fid);
+
     % call exectuable (blocking)
-    copyfile(fullfile(simdir, 'fullwave2_executable'))
+    disp("Running from " + pwd());
     [err, res] = system('./fullwave2_executable'); % TODO: not sure how to call from Windows/Mac?
 
     disp("Received code " + err)
     disp(res)
 
+    % return to original directory
+    cd(ogdir);
+
     % move output files to simulation output directory 
-    copyfile('genout.dat', fullfile(outdir));
+    copyfile(fullfile(tmp, 'genout.dat'), fullfile(outdir));
 
     % TODO: get memory map for the data if an output was requested?
 
@@ -64,8 +73,8 @@ catch ME, getReport(ME) % show the error
     
 end
 
-% move back to original folder
+% ensure back in the original folder
 cd(ogdir);
 
 % force delete the simulation folder
-suc = rmdir(tmp, 's'); % recursive
+suc = rmdir(tmp, 's'); % cleanup: recursive deletion
