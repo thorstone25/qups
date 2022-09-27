294976bc578b0c0fe99fdcf1a41a00c3419de609
Thurston <tbrevett@live.com>
2022-09-27 01:14:17 -0700

    slsc(): updated documentation; 'ensemble' method parallelized.

--- kern/slsc.m
+++ kern/slsc.m
@@ -2,9 +2,9 @@ function z = slsc(x, dim, L, method, kdim)
 % SLSC - Short-lag Spatial Coherence 
 %
 % z = SLSC(x) computes the short-lag spatial coherence (SLSC) across the
-% data x. x can be any ND-array.
+% data ND-array x.
 %
-% z = SLSC(x, dim) operates in dimension dim. The default is the first 
+% z = SLSC(x, dim) operates in dimension dim. The default is the last 
 % non-singleton dimension.
 % 
 % z = SLSC(x, dim, L) uses up to L lags. If L is an array of indices, the
@@ -12,70 +12,119 @@ function z = slsc(x, dim, L, method, kdim)
 % including 0 e.g. L = [0 5]. The default is 
 % max(1, floor(1/4 * size(x,dim)));
 % 
-% z = SLSC(x, dim, L, method) uses the specified method. Must be one of
-% {"ensemble"* | "average"}.
+% z = SLSC(x, dim, L, method) uses the specified method. Must be one
+% of {"ensemble" | "average"*}.
 % 
-% z = SLSC(x, dim, L, method, kdim) designates a temporal sample dimension
-% used in calculating the average SLSC. The default is ndims(x) + 1.
+% z = SLSC(x, dim, L, method, kdim) interprets kdim as the time dimension. 
+% When dimension kdim is non-singular, samples in time are treated as part
+% of the same correlation.
 % 
-% Example:
-% 
-% % generate a sine wave with random phase error
-% T = 2^4; % number of samples
-% fc = 2; % normalized frequency
-% t = (0:T-1)'/T; % time vector
-% phi = pi/2*rand([1,64]); % random phase differences
-% x = exp(1j*(2*pi*fc*t + phi)); % sine wave
-%
-% % get the SLSC across the phases
-% z = slsc(x, 2, [0 3], 'average')
+% About: 
+% The short-lag spatial coherence measures how similar signals are across a
+% short distance. In medical ultrasound, speckle will exhibit low coherence
+% over a short region whereas tissue walls may exhibit high spatial
+% coherence.
 % 
 % References:
 % D. Hyun, A. C. Crowley and J. J. Dahl, 
 % "Efficient Strategies for Estimating the Spatial Coherence of Backscatter,"
 % in IEEE Transactions on Ultrasonics, Ferroelectrics, and Frequency Control, 
 % vol. 64, no. 3, pp. 500-513, March 2017.  
-% doi: 10.1109/TUFFC.2016.2634004
+% doi: <a href="matlab:web('doi.org/10.1109/TUFFC.2016.2634004')">10.1109/TUFFC.2016.2634004</a>
 %
 % M. A. Lediju, G. E. Trahey, B. C. Byram and J. J. Dahl, 
 % "Short-lag spatial coherence of backscattered echoes: imaging characteristics," 
 % in IEEE Transactions on Ultrasonics, Ferroelectrics, and Frequency Control, 
 % vol. 58, no. 7, pp. 1377-1388, July 2011.  
-% doi: 10.1109/TUFFC.2011.1957
+% doi: <a href="matlab:web('doi.org/10.1109/TUFFC.2011.1957')">10.1109/TUFFC.2011.1957</a>
 %
-% See also APCOHERENCE
+% Example:
+% % This example requires kWave
+% if ~exist('kWaveGrid', 'class')
+%   warning('kWave must be on the path to run this example.');
+% end
+%
+% % Setup a system
+% sscan = ScanCartesian(...
+%   'x', 1e-3*linspace(-20, 20, 1+40*2^3), ...
+%   'z', 1e-3*linspace(-02, 38, 1+40*2^3) ...
+%   );
+% xdc = TransducerArray();
+% seq = SequenceRadial('angles', -21 : 1.5 : 21, 'ranges', 1); % plane-waves
+% us = UltrasoundSystem('scan', sscan, 'xdc', xdc, 'sequence', seq);
+% 
+% % Create a Medium to simulate
+% [c, rho] = deal(1500*ones(sscan.size), 1000*ones(sscan.size));
+% rho = rho + 15 * (rand(sscan.size)-0.5); % 1% variation in the density
+% [Xg, ~, Zg] = sscan.getImagingGrid();
+% for zj = 5:5:30
+%   rho(Xg == 0 & Zg == 1e-3*zj) = 1000*2; % double the density at these depths
+% end
+% med = Medium.Sampled(sscan, c, rho, 'c0', 1500);
+% 
+% % Setup a compute cluster
+% clu = parcluster('local');
+% clu.NumWorkers = 1 + 2*gpuDeviceCount;
+% clu.NumThreads = 4*gpuDeviceCount;
+%
+% % Simulate the ChannelData
+% if gpuDeviceCount, dtype = 'gpuArray-single';
+% else, dtype = 'single'; end
+% [job, rfun] = kspaceFirstOrder(us, med, sscan, 'DataCast', dtype, 'parenv', clu);
+% submit(job); % launch
+% wait(job);
+% chd = rfun(job); % read data into a ChannelData object
+% 
+% % Pre-process the data
+% chd = hilbert(chd);
+%
+% % generate an image, preserving the receive dimension
+% lambda = med.c0 / us.xdc.fc; % wavelength
+% us.scan.dz = lambda / 4; % axial resolution
+% us.scan.dx = lambda / 4; % lateral resolution
+% b = DAS(us, chd, 'keep_rx', true);
+% mdim = ndims(b); % the last dimension is the receiver dimension
+% 
+% % compute and show the SLSC image
+% z = slsc(b, mdim);
+% figure;
+% imagesc(us.scan, real(z));
+% colorbar;
+% 
+% See also COHFAC
 
 % defaults
 arguments
     x
-    dim (1,1) {mustBePositive, mustBeInteger} = prod(find(size(x,1:ndims(x)) ~= 1, 1, 'first')); % lag dimension
+    dim (1,1) {mustBePositive, mustBeInteger} = prod(find(size(x,1:ndims(x)) ~= 1, 1, 'last')); % lag dimension
     L (1,:) {mustBeNonnegative, mustBeInteger} = max(1, floor(size(x,dim)/4)); % lags or maximum lag
     method (1,1) string {mustBeMember(method, ["average", "ensemble"])} = "average"; % average or ensemble estimator
-    kdim (1,1) {mustBePositive, mustBeInteger} = ndims(x) + 1; % time sample dimension
+    kdim (1,1) {mustBePositive, mustBeInteger} = ndims(x)+1; % time samples dimension
 end
 
 % default parpool: this is data heavy, so only use a threadpool
 % don't use any pool for gpu data
 if isa(gcp('nocreate'), 'parallel.threadPool') ...
-        && ~isa(x, 'gpuArray') && ~isa(x, 'tall'), 
+        && ~isa(x, 'gpuArray') && ~isa(x, 'tall') 
     clu = gcp(); else, clu = 0; 
 end
 
 % get weighting filter across receiver pairs
 A = gather(size(x,dim)); % the full size of the aperture
 [M, N] = ndgrid(1:A, 1:A);
-H = abs(M - N); % lags for each receiver/cross-receiver pair
-if isscalar(L), lags = 1:L; else, lags = L; end % chosen lags for adding (i.e. the short lags)
-S = ismember(H,lags); % selection mask for the lags
-L = length(lags); % number of lags - this normalizes the sum of the average estimates
+H = abs(M - N); % lags for each receiver/cross-receiver pair (M x M')
+lags = L; % if isscalar(L), lags = 1:L; else, lags = L; end % chosen lags for adding (i.e. the short lags)
+S = ismember(H,lags); % selection mask for the lags (M x M') - true if (rx, xrx) is a short lag
+L = numel(lags); % number of (active) lags - this normalizes the sum of the average estimates
 K = gather(size(x,kdim)); % number of samples in time
 
 % choose average or ensemble
 switch method
     case "average"
-        % normalize magnitude per sample (averaging)
+        % normalize magnitude per time sample (averaging)
         x = x ./ vecnorm(x,2,kdim) / K;
         x(isnan(x)) = 0; % 0/0 -> 0
+        
         % TODO: test if norm(x) close to zero instead of assuming all nans
         % are from computing 0/0 
 
@@ -83,12 +132,13 @@ switch method
         W = S ./ (A - H) / 2 / L; % weights per pair (debiased, pos & neg averaged, multi-correlation-averaged)
 
         % place weights as cross receiver over cells, receiver in dim
-        W = num2cell(shiftdim(W, -(dim-2)), dim);
+        W = num2cell(shiftdim(W, -(dim-2)), dim); % ... x (M') x {M}
 
         % compute product and sum per cross-receiver
         z = 0;
         parfor (i = 1:A, clu)
-            z = z + sum(W{i} .* conj(sub(x,i,dim)) .* x, [dim, kdim], 'omitnan');
+            xc = sub(x,i,dim); % cross-receiver
+            z = z + sum(W{i} .* conj(xc) .* x, [dim, kdim], 'omitnan');
         end
 
     case "ensemble"
@@ -101,8 +151,7 @@ switch method
 
         % compute and sum for each cross-receiver
         [z, a, b] = deal(0);
-        %parfor (i = 1:numel(W), clu)
-        for i = 1:A
+        parfor (i = 1:A, clu)
             w = W{i}; % weight for this set of indices
             xc = sub(x,i,dim); % cross-receiver
             z = z + sum(w .* conj(xc) .* x , [dim, kdim],'omitnan'); % inner-product
