ba355950e41ad99478eb06dbd3c3a5e0bcaf7da0
Thurston <tbrevett@gmail.com>
2023-04-19 21:09:13 -0700

    Adding SequenceGeneric and TransducerGeneric classes.

---
+++ src/SequenceGeneric.m
@@ -0,0 +1,232 @@
+classdef SequenceGeneric < Sequence
+    % SEQUENCEGENERIC - Class defining transmit sequences with arbitrary
+    % delays
+    %
+    % A SEQUENCEGENERIC object defines a generic pulse sequence where an 
+    % arbitrary set of apodization weight and pulse delays is defined per 
+    % element per pulse. The same waveform must be sent for all elements 
+    % and pulses. Delays and apodization matrices must have compatiable 
+    % sizing for a given Transducer when used with other methods.
+    % 
+    % See also SEQUENCE SEQUENCERADIAL WAVEFORM
+    
+    properties
+        % APOD - Apodization function/matrix definition
+        %
+        % SEQUENCEGENERIC.APOD specifies the apodization weights a as 
+        % either a (N x S) matrix, or a function that takes a Transducer 
+        % and a Sequence and returns a (N x S) matrix of weights where N is
+        % the number of elements of the corresponding Transducer and S is 
+        % the number of pulses.
+        %
+        % Example:
+        % % Create a hadamard encoding sequence
+        % seq = SequenceGeneric('type', 'FSA');
+        % seq.apod = @(tx, seq) hadamard(tx.numel);
+        % seq.del  = @(tx, seq) zeros(tx.numel);
+        %
+        % % Get a default system and scatterers
+        % us = UltrasoundSystem('sequence', seq);
+        % us.sequence.numPulse = us.tx.numel; % set the number of pulses
+        % scat = Scatterers('pos', [0;0;30e-3]);
+        % 
+        % % get the ChannelData
+        % chdh = greens(us, scat);
+        %
+        % % decode the data via refocus
+        % chd = refocus(us, chdh, 'gamma', 0); % don't use regularization
+        % us.sequence.apod = @(tx, seq) eye(tx.numel); % specify FSA apodization
+        % 
+        % % beamform and display the image
+        % figure; 
+        % imagesc(mod2db(DAS(us, chd)));
+        % caxis(max(caxis) + [-60 0]);
+        % 
+        % See also APODIZATION SEQUENCEGENERIC.DEL
+        apod 
+        
+        % DEL - Delay function/matrix definition
+        %
+        % SEQUENCEGENERIC.DEL specifies the delays as either a (N x S) matrix, or 
+        % a function that takes a Transducer and a Sequence and returns a 
+        % (N x S) matrix of delays where N is the number of elements and S 
+        % is the number of pulses.
+        %
+        % Example:
+        % % Create a random phase sequence
+        % del  = @(tx, seq) (randi([0,3],tx.numel) - 1.5) / 4 / tx.fc;
+        % apod = @(tx, seq) ones(size(tx.numel));
+        % seq = SequenceGeneric('del', del, 'apod', apod);
+        %
+        % % Get a default system and scatterers
+        % us = UltrasoundSystem('sequence', seq);
+        % scat = Scatterers('pos', [0;0;30e-3]);
+        % 
+        % % get the ChannelData
+        % chdh = greens(us, scat);
+        %
+        % % decode the data via refocus
+        % chd = refocus(us, chdh, 'gamma', 0); % don't use regularization
+        % us.sequence.del  = zeros(chd.N); % set as a FSA sequence
+        % us.sequence.apod =   eye(chd.N); % set as a FSA sequence
+        % 
+        % % beamform and display the image
+        % figure; 
+        % imagesc(mod2db(DAS(us, chd)));
+        % caxis(max(caxis) + [-60 0]);
+        % 
+        % See also DELAYS SEQUENCEGENERIC.APOD
+        del
+    end
+    properties(Hidden)
+        tscale = 1;
+    end
+    methods
+        % constructor
+        function seq = SequenceGeneric(seq_kwargs, gen_kwargs)
+            % SEQUENCEGENERIC - SequenceGeneric constructor
+            %
+            % seq = SEQUENCEGENERIC() constructs a SequenceGeneric
+            %
+            % seq = SEQUENCEGENERIC(Name, Value, ...) uses Name/Value pair
+            % arguments to construct a SequenceGeneric
+            %
+            % seq = SEQUENCEGENERIC('numPulse', N) defines a generic 
+            % sequence for a transducer with N elements.
+            %
+            % seq = SEQUENCEGENERIC(..., 'c0', c0) sets the beamforming
+            % sound speed to c0. 
+            %
+            % seq = SEQUENCEGENERIC(..., 'pulse', wv) defines the 
+            % transmitted pulse to be the Waveform wv.
+            %
+            % See also SEQUENCE SEQUENCERADIAL WAVEFORM
+            arguments
+                seq_kwargs.c0 (1,1) double
+                seq_kwargs.pulse (1,1) Waveform
+                seq_kwargs.numPulse (1,1) double {mustBeInteger}
+                gen_kwargs.apod {mustBeA(gen_kwargs.apod, ["function_handle", "numeric", "logical"])} = @(tx, seq) eye(tx.numel);
+                gen_kwargs.del {mustBeA(gen_kwargs.del, ["function_handle", "numeric", "logical"])} = @(tx, seq) zeros(tx.numel);
+            end
+
+            % initialize the Sequence
+            seq_kwargs.type = 'FSA'; % always use FSA format.
+            seq_args = struct2nvpair(seq_kwargs);
+            seq@Sequence(seq_args{:});
+            
+            % initialize
+            for s = string(fieldnames(gen_kwargs))', seq.(s) = gen_kwargs.(s); end
+        end
+
+        function s = obj2struct(seq)
+            arguments, seq Sequence {mustBeScalarOrEmpty}; end
+            s = obj2struct@Sequence(seq); % call superclass conversion
+        end
+        
+        % scaling
+        function seq = scale(seq, kwargs)
+            % SCALE - Scale units
+            %
+            % seq = SCALE(seq, 'dist', factor) scales the distance of the
+            % properties by factor. This can be used to convert from meters
+            % to millimeters for example.
+            %
+            % seq = SCALE(seq, 'time', factor) scales the temporal
+            % properties by factor. This can be used to convert from
+            % seconds to microseconds and hertz to megahertz.
+            %
+            % Example:
+            %
+            % % Create a SequenceGeneric
+            % tau = [-2:1:2; zeros(1,5); 2:-1:-2]'; % 5 x 3 delays
+            % seq = SequenceGeneric('c0', 1500, 'del', tau, 'apod', ones(size(tau))); % m, s, Hz
+            %
+            % % convert from meters to millimeters, hertz to megahertz
+            % seq = scale(seq, 'dist', 1e3, 'time', 1e6); % mm, us, MHz
+            % seq.c0 % in mm/us
+            % seq.del % in us
+            %
+            %
+            arguments
+                seq Sequence
+                kwargs.dist (1,1) double
+                kwargs.time (1,1) double
+            end
+
+            % call superclass conversion
+            args = struct2nvpair(kwargs);
+            seq = scale@Sequence(args{:});
+
+            % record scaling for delays
+            seq.tscale = seq.tscale .* kwargs.time;
+        end
+    end
+    
+    % conversion methods
+    methods
+        function seq = getUSTBSequence(self, xdc, t0)
+            % GETUSTBSEQUENCE - Get a USTB/UFF uff.sequence object
+            %
+            % seq = GETUSTBSEQUENCE(self, xdc, t0) creates a USTB
+            % compatible sequence object from the QUPS Sequence object
+            % where xdc is a QUPS transducer and t0 is the start time in
+            % the QUPS coordinate system.
+            %
+            % See also TRANSDUCER/GETUSTBPROBE
+            arguments
+                self (1,1) Sequence
+                xdc (1,1) Transducer
+                t0 (1,1) double % bulk offset of the data
+            end
+
+            error('Not implemented.');
+
+            % initialize all wave objects
+            N = self.numPulse;
+            for n = N:-1:1, seq(n) = uff.wave(); end
+            
+            % set the common settings
+            [seq.probe] = deal(xdc.getUSTBProbe());
+            [seq.sound_speed] = deal(self.c0);
+            
+            switch self.type
+                case {'PW'}
+                    [seq.wavefront] = deal(uff.wavefront.plane);
+                    theta = atan2(self.focus(1,:),self.focus(3,:));
+                    phi   = atan2(self.focus(2,:),hypot(self.focus(1,:),self.focus(3,:)));
+                    for n=1:N, seq(n).source = uff.point(...
+                            'azimuth', theta(n), ...
+                            'elevation', phi(n), ...
+                            'distance', inf ...
+                            );
+                    end
+                    [seq.delay] = deal(t0);
+                    
+                case {'FSA'}
+                    p = xdc.positions();
+                    [seq.wavefront] = deal(uff.wavefront.spherical);
+                    for n=1:N, seq(n).source.xyz = p(:,n).'; end
+                    for n=1:N, seq(n).delay = p(:,n)/self.c0 + t0; end
+                    
+                case {'VS'}
+                    [seq.wavefront] = deal(uff.wavefront.spherical);
+                    for n=1:N, seq(n).source.xyz = self.focus(:,n).'; end
+                    [seq.delay] = deal(t0);
+                    
+            end   
+        end
+    end
+    
+    % temporal response methods
+    methods   
+        function t0 = t0Offset(seq), arguments, seq SequenceGeneric, end, t0 = 0; end
+
+        function tau = delays(seq, tx)
+            arguments, seq SequenceGeneric, tx Transducer, end
+
+            % call superclass for delays, then scale time
+            tau = seq.tscale .* delays@Sequence(seq, tx); 
+
+        end
+    end    
+end
---
+++ src/TransducerGeneric.m
@@ -0,0 +1,214 @@
+% TRANSDUCERGENERIC - Generic Transducer Array class
+% 
+% A TransducerGeneric defines a generic transducer by the position and
+% orientations of its elements
+% 
+% See also TRANSDUCER TRANSDUCERARRAY TRANSDUCERCONVEX TRANSDUCERMATRIX
+
+classdef TransducerGeneric < Transducer
+
+    properties
+        pos (3,:) % element positions
+        az  (1,:) % element azimuth angle (deg)
+        el  (1,:) % element elevation angle (deg)
+    end
+    
+    % constructor 
+    methods(Access=public)
+        function xdc = TransducerGeneric(gen_args, xdc_args)
+            % TRANSDUCERGENERIC - TransducerGeneric constructor
+            %
+            % xdc = TRANSDUCERGENERIC(Name, Value, ...) constructs a
+            % TransducerGeneric using name/value pair arguments.
+            %
+            % See also TRANSDUCERARRAY TRANSDUCERCONVEX
+            arguments
+                gen_args.pos (3,:) double = [0;0;0]
+                gen_args.az (1,:) double = 0
+                gen_args.el (1,:) double = 0
+                xdc_args.?Transducer
+            end
+
+            % --- check the sizing of the inputs --- %
+            N = size(gen_args.pos,2); % number of element positions
+
+            % azimuth angles
+            Na = numel(gen_args.az); % get current length
+            if Na == 1, gen_args.az = repmat(gen_args.az, [1 N]); end % if scalar, make vector
+            assert(any(numel(gen_args.az) == N), 'The number of angles must be either scalar, or equal to the number of positions.');
+
+            % elevation angles
+            Ne = numel(gen_args.el); % get current length
+            if Ne == 1, gen_args.el = repmat(gen_args.el, [1 N]); end % if scalar, make vector
+            assert(any(numel(gen_args.el) == N), 'The number of angles must be either scalar, or equal to the number of positions.');
+
+            % initialize the Transducer
+            xdc_args = struct2nvpair(xdc_args);
+            xdc@Transducer(xdc_args{:}) 
+            
+            % initialize the TransducerGeneric 
+            for f = string(fieldnames(gen_args))'
+                xdc.(f) = gen_args.(f);
+            end
+        end
+    end
+    
+    % manipulation
+    methods
+        % scaling
+        function xdc = scale(xdc, kwargs)
+            % SCALE - Scale units
+            %
+            % xdc = SCALE(xdc, 'dist', factor) scales the distance of the
+            % properties by factor. This can be used to convert from meters
+            % to millimeters for example.
+            %
+            % xdc = SCALE(xdc, 'time', factor) scales the temporal
+            % properties by factor. This can be used to convert from
+            % seconds to microseconds and hertz to megahertz.
+            %
+            % Example:
+            %
+            % % Create a TransducerGeneric
+            % xdc = TransducerGeneric('fc', 5e6, 'pos', [0.3e-3; 0; 0.5e-3]); % m, s, Hz
+            %
+            % % convert from meters to millimeters, hertz to megahertz
+            % xdc = scale(xdc, 'dist', 1e3, 'time', 1e6) % mm, us, MHz
+            % xdc.fc % in MHz
+            % xdc.pos % in mm
+            %
+            %
+
+            arguments
+                xdc TransducerGeneric
+                kwargs.dist (1,1) double
+                kwargs.time (1,1) double
+            end
+            args = struct2nvpair(kwargs); % get the arguments as Name/Value pairs
+            xdc = scale@Transducer(xdc, args{:}); % call superclass method
+            if isfield(kwargs, 'dist')
+                w = kwargs.dist;
+                % scale distance (e.g. m -> mm)
+                [xdc.pos] = deal(w*xdc.pos);
+            end
+        end
+    end
+    
+    % define abstract methods
+    methods
+        function p = positions(xdc), p = xdc.pos; end
+        function [theta, phi, normal, width, height] = orientations(xdc)
+            theta = xdc.az;
+            phi   = xdc.el;
+            ZERO  = zeros(size(theta));
+            normal     = [cosd(phi).*sind(theta); sind(phi);  cosd(phi).*cosd(theta)];
+            width      = [cosd(phi).*cosd(theta); sind(phi); -cosd(phi).*sind(theta)];
+            height     = [sind(phi).*sind(theta); cosd(phi);  sind(phi).*cosd(theta)];
+        end
+    end
+
+    % Fullwave functions
+    methods
+        function xdc = getFullwaveTransducer(self, sscan)
+            error('Not implemented.');
+        end
+    end
+
+    % SIMUS conversion functions
+    methods
+        function p = getSIMUSParam(self)
+            error("SIMUS does not support a TransducerGeneric.");
+            % TODO: error if origin not at 0.
+        end
+    end
+
+    % Field II conversion function
+    methods(Access=public)
+        function aperture = getFieldIIAperture(self, focus, element_sub_divisions)
+            error('Not implemented.');
+            if nargin < 2 || isempty(focus), focus = [0 0 realmax('single')]; end % ~ 0-deg plane wave
+            if nargin < 3, element_sub_divisions = [1,3]; end % no sub divisions
+            % TODO: error if origin not at 0.
+                        
+            % Field II parameters
+            xdc_lin_array_params = { ...
+                self.numel, ...
+                self.width, ...
+                self.height,...
+                self.kerf, ...
+                element_sub_divisions(1), ...
+                element_sub_divisions(2), ...
+                reshape(focus, 1, []),...
+                };
+            
+            % ensure double type
+            xdc_lin_array_params = cellfun(@double, xdc_lin_array_params, 'UniformOutput', false);
+            xdc_lin_array_params = cellfun(@gather, xdc_lin_array_params, 'UniformOutput', false);
+            
+            % Generate aperture for emission
+            try evalc('field_info'); catch, field_init(-1); end
+            aperture = xdc_linear_array(xdc_lin_array_params{:});
+        end        
+    end
+    
+    % USTB conversion function
+    methods
+        function probe = getUSTBProbe(self)
+            error('Not implemented :(.');
+            probe = uff.linear_array(...
+                'N', self.numel, ...
+                'pitch', self.pitch, ...
+                'element_width', self.width, ...
+                'element_height', self.height, ...
+                'origin', uff.point('xyz', self.offset(:)') ...
+                );
+        end
+    end
+
+    methods(Static)
+        function xdc = Verasonics(Trans, c0)
+            arguments
+                Trans struct
+                c0 (1,1) double = 1540
+            end
+            error('Not implemented.')
+
+            % determine the scaling of the properties
+            switch Trans.units
+                case 'wavelengths', scale = c0 / Trans.frequency * 1e-6; % lambda -> m
+                case 'mm', scale = 1e-3; % mm -> m
+            end
+
+            % parse the impulse response
+            h = Trans.IR1wy; % impulse response
+            t0 = - (argmax(hilbert(h))-1) / 250e6; % offset to peak time
+            wv = Waveform('t', t0 + (0:numel(h)-1) / 250e6, 'samples',h); % impulse response
+            
+            % set relevant properties
+            xdc = TransducerArray(...
+                'fc', 1e6*Trans.frequency, ... % Transducer center frequency [Hz]
+                'bw', 1e6*Trans.Bandwidth([1 end]), ... % bandwidth [Hz]
+                'impulse', wv, ... % impulse response function
+                'width', scale*Trans.elementWidth, ... % linear kerf
+                'height', 1e-3*Trans.elevationApertureMm, ... % Height of element [m]
+                'numel', Trans.numelements, ... % number of elements
+                'pitch', 1e-3*Trans.spacingMm, ... % probe pitch [m]
+                'el_focus', 1e-3*Trans.elevationFocusMm ... % elevation focal depth
+                );
+        end
+        function xdc = UFF(probe)
+            arguments
+                probe (1,1) {mustBeA(probe, 'uff.linear_array')}
+            end
+            error('Not implemented.');
+            if isempty(probe.origin), probe.origin.xyz = [0;0;0]; end % force 0 if empty
+            xdc = TransducerArray(...
+                "pitch", probe.pitch, ...
+                "width", probe.element_width, ...
+                "height", probe.element_height, ...
+                "numel", probe.N_elements, ...
+                "offset", - probe.origin.xyz ...
+                );
+        end
+    end
+end
