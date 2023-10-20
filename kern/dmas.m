1722a74a1ce2692e79a91ed6a34fea164251c645
Thurston <tbrevett@gmail.com>
2023-10-20 07:27:35 -0700

    dmas() : added lag argument to select aperture pairs.

--- kern/dmas.m
+++ kern/dmas.m
@@ -1,13 +1,23 @@
-function b = dmas(bn, dim)
+function b = dmas(bn, dim, L)
 % DMAS - Delay-Multiply-And-Sum (DMAS)
 % 
 % b = DMAS(bn) computes the delay-multiply-and-sum image b from the b-mode 
-% image per receiver bn.
+% image per receive element bn.
 %
 % b = DMAS(bn, dim) specifies the receive dimension of bn. The default is 
 % the last non-singular dimension of bn.
+%
+% b = DMAS(bn, dim, lags) specifies the element lags to be included. The
+% default is (1 : size(bn,dim) - 1).
 % 
-% About: Delay-multiply-and-sum is a contrast enhancement method. 
+% b = DMAS(bn, dim, L) where L is a scalar selects lags 1:L. To select only
+% lag L, specify an array which includes 0 as in lags = [0, L]. 
+%
+% About: Delay-multiply-and-sum is a contrast enhancement method based on
+% correlations between pairs of signals across the aperture.
+%
+% For complex numbers, this implementation preserves the phase in the
+% multiplication stage, which is the complex analog of preserving the sign.
 %
 % References:
 % [1] G. Matrone, A. S. Savoia, G. Caliano and G. Magenes, 
@@ -20,7 +30,6 @@ function b = dmas(bn, dim)
 % us = UltrasoundSystem(); % get a default system
 % us.scan = ScanCartesian('x', (-20 : 20)*1e-3, 'z', (0 : 50) * 1e-3);
 % [us.scan.dx, us.scan.dz] = deal(us.lambda / 8);
-%
 % 
 % % Generate Scatterers
 % scat = Scatterers('pos', 1e-3*[0,0,30]', 'c0', us.sequence.c0);
@@ -58,11 +67,13 @@ function b = dmas(bn, dim)
 arguments
     bn {mustBeNumeric}
     dim {mustBeInteger, mustBePositive} = find(size(bn) ~= 1, 1, 'last');
+    L  (1,:) {mustBeInteger, mustBeNonnegative} = 1:size(bn, dim)-1 % lags
 end
 
 b = 0; % init
 N = size(bn, dim); % aperture length
-for i = 1:N-1 % multiply and sum along all non-identical pairs
+if isscalar(L), lags = 1:L; else; lags = intersect(1:N-1, L); end
+for i = lags % multiply and sum along all non-identical pairs
     b = b + sum(sub(bn,1:N-i,dim) .* sub(bn,1+i:N,dim), dim);
 end
 
