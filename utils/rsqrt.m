8fa272e5b84e72a26c7f96365186add2d5b7cc14
thorstone25 <tbrevett@gmail.com>
2024-01-08 12:46:55 -0800

    rsqrt() : update documentation.

--- utils/rsqrt.m
+++ utils/rsqrt.m
@@ -1,1 +1,18 @@
-function x = rsqrt(x), if min(x,[],'all', 'omitnan') < 0, x = rdivide(1, sqrt(complex(x))); else,  x = rdivide(1, sqrt(x)); end
+function x = rsqrt(x)
+% RSQRT - Reciprical square root
+%
+% y = rsqrt(x) returns the reciprocal square root of x by computing
+% 1 ./ sqrt(x), while handling MATLAB casting issues for gpuArrays or
+% integer types. 
+% 
+% gpuArray types are cast to complex if x contains negative values. 
+% Integer types are upcast to double.
+%
+% See also: SQRT
+
+arguments, x {mustBeNumeric}, end
+if isinteger(x),    x = cast(x,'double'); end % cannot rsqrt integers
+if any(x < 0,'all'),x = 1 ./ sqrt(complex(x)); % must be done manually for gpuArrays
+else,               x = 1 ./ sqrt(        x );
+end
+end
