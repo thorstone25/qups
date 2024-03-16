320a9e913f4e8ee690e0adbc9954b5f60f32f31f
Thurston <tbrevett@stanford.edu>
2024-03-15 22:58:19 -0700

    SequenceGeneric() : removing unused method; update examples.

--- src/SequenceGeneric.m
+++ src/SequenceGeneric.m
@@ -28,6 +28,13 @@ classdef SequenceGeneric < Sequence
             % seq = SEQUENCEGENERIC(..., 'pulse', wv) defines the
             % transmitted pulse to be the Waveform wv.
             %
+            % Example:
+            % N   = 64; % elems
+            % apd = Sequence.apWalking(N, N/2, N/8); % elems x transmits
+            % del = 0 * apd; % no delays
+            % M   = size(apd, 2); % number of transmits
+            % seq = SequenceGeneric('apd', apd, 'del', del, 'numPulse', M)
+            % 
             % See also SEQUENCE SEQUENCERADIAL WAVEFORM
             arguments
                 kwargs.c0 (1,1) double
@@ -52,61 +59,6 @@ classdef SequenceGeneric < Sequence
             kwargs.type = 'FSA'; % always use FSA format.
             seq_args = struct2nvpair(kwargs);
             seq@Sequence(seq_args{:});
-        end
-    end
-
-    % conversion methods
-    methods
-        function seq = getUSTBSequence(self, xdc, t0)
-            % GETUSTBSEQUENCE - Get a USTB/UFF uff.sequence object
-            %
-            % seq = GETUSTBSEQUENCE(self, xdc, t0) creates a USTB
-            % compatible sequence object from the QUPS Sequence object
-            % where xdc is a QUPS transducer and t0 is the start time in
-            % the QUPS coordinate system.
-            %
-            % See also TRANSDUCER/GETUSTBPROBE
-            arguments
-                self (1,1) Sequence
-                xdc (1,1) Transducer
-                t0 (1,1) double % bulk offset of the data
-            end
-
-            error('Not implemented.');
-
-            % initialize all wave objects
-            N = self.numPulse;
-            for n = N:-1:1, seq(n) = uff.wave(); end
-
-            % set the common settings
-            [seq.probe] = deal(xdc.getUSTBProbe());
-            [seq.sound_speed] = deal(self.c0);
-
-            switch self.type
-                case {'PW'}
-                    [seq.wavefront] = deal(uff.wavefront.plane);
-                    theta = atan2(self.focus(1,:),self.focus(3,:));
-                    phi   = atan2(self.focus(2,:),hypot(self.focus(1,:),self.focus(3,:)));
-                    for n=1:N, seq(n).source = uff.point(...
-                            'azimuth', theta(n), ...
-                            'elevation', phi(n), ...
-                            'distance', inf ...
-                            );
-                    end
-                    [seq.delay] = deal(t0);
-
-                case {'FSA'}
-                    p = xdc.positions();
-                    [seq.wavefront] = deal(uff.wavefront.spherical);
-                    for n=1:N, seq(n).source.xyz = p(:,n).'; end
-                    for n=1:N, seq(n).delay = p(:,n)/self.c0 + t0; end
-
-                case {'VS','DV','FC'}
-                    [seq.wavefront] = deal(uff.wavefront.spherical);
-                    for n=1:N, seq(n).source.xyz = self.focus(:,n).'; end
-                    [seq.delay] = deal(t0);
-
-            end
         end
     end
 
