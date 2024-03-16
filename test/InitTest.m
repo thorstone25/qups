0f3f00678a972d245f1f4f13cdc2a4bef9c3405c
Thurston <tbrevett@stanford.edu>
2024-03-16 01:56:10 -0700

    InitTest : tests heterogenous array, scale() support.

--- test/InitTest.m
+++ test/InitTest.m
@@ -19,39 +19,44 @@ classdef (TestTags = ["Github", "full", "build"]) InitTest < matlab.unittest.Tes
             % INITXDC - Assert that Transducer constructors initialize
             % without arguments
             import matlab.unittest.constraints.IsInstanceOf;
-            test.assertThat(TransducerArray() , IsInstanceOf('Transducer'));
-            test.assertThat(TransducerConvex(), IsInstanceOf('Transducer'));
-            test.assertThat(TransducerMatrix(), IsInstanceOf('Transducer'));
-            test.assertThat(TransducerGeneric(), IsInstanceOf('Transducer'));
+            xdcs = [TransducerArray, TransducerConvex, TransducerMatrix, TransducerGeneric]; % init / hetero
+            arrayfun(@(xdc) test.assertThat(xdc, IsInstanceOf('Transducer')), xdcs); % class maintained
+            arrayfun(@(xdc) scale(xdc, 'dist', 1e3, 'time', 1e6), xdcs, "UniformOutput",false); % can scale
+            xdcs(end+2) = xdcs(1); % implicit empty value            
        end
-        function initchd(test)
-            % INITCHD - Assert that a ChannelData constructor initializes
-            % without arguments
-            import matlab.unittest.constraints.IsInstanceOf;
-            test.assertThat(ChannelData(), IsInstanceOf('ChannelData'));
-        end
         function initseq(test)
             % INITSEQ - Assert that Sequence constructors initialize
             % without arguments
             import matlab.unittest.constraints.IsInstanceOf;
-            test.assertThat(Sequence(), IsInstanceOf('Sequence'));
-            test.assertThat(SequenceRadial(), IsInstanceOf('Sequence'));
-            test.assertThat(SequenceGeneric(), IsInstanceOf('Sequence'));
+            seqs = [Sequence(), SequenceRadial(), SequenceGeneric()];
+            arrayfun(@(seq) test.assertThat(seq, IsInstanceOf('Sequence')), seqs);
+            arrayfun(@(scn) scale(scn, 'dist', 1e3), seqs, "UniformOutput",false); % can scale
+            seqs(end+2) = seqs(1); % implicit empty value 
         end
         function initscan(test)
             % INITSCAN - Assert that Scan constructors initialize
             % without arguments
             import matlab.unittest.constraints.IsInstanceOf;
-            test.assertThat(ScanCartesian(), IsInstanceOf('Scan'));
-            test.assertThat(ScanPolar(), IsInstanceOf('Scan'));
-            test.assertThat(ScanSpherical(), IsInstanceOf('Scan'));
-            test.assertThat(ScanGeneric(), IsInstanceOf('Scan'));
+            scns = [ScanCartesian(), ScanPolar(), ScanSpherical(),ScanGeneric()]; % init / hetero
+            arrayfun(@(scn) test.assertThat(scn, IsInstanceOf('Scan')), scns);
+            arrayfun(@(scn) scale(scn, 'dist', 1e3), scns, "UniformOutput",false); % can scale
+            scns(end+2) = scns(1); % implicit empty value            
+        end
+        function initchd(test)
+            % INITCHD - Assert that a ChannelData constructor initializes
+            % without arguments
+            import matlab.unittest.constraints.IsInstanceOf;
+            chd = ChannelData();
+            test.assertThat(chd, IsInstanceOf('ChannelData'));
+            scale(chd, 'time', 1e6);
         end
         function initus(test)
             % INITUS - Assert that an UltrasoundSystem constructor
             % initializes without arguments
             import matlab.unittest.constraints.IsInstanceOf;
-            test.assertThat(UltrasoundSystem(), IsInstanceOf('UltrasoundSystem'));
+            us = UltrasoundSystem();
+            test.assertThat(us, IsInstanceOf('UltrasoundSystem'));
+            us = scale(us, "dist",1e3, "time",1e6);
         end
         function initmedscat(test)
             % INITMEDSCAT - Assert that a Scatterers and Mediums
@@ -59,7 +64,10 @@ classdef (TestTags = ["Github", "full", "build"]) InitTest < matlab.unittest.Tes
 
             import matlab.unittest.constraints.IsInstanceOf;
             test.assertThat(Scatterers(), IsInstanceOf('Scatterers'));
-            test.assertThat(Medium(), IsInstanceOf('Medium'));
+            test.assertThat(Medium(),     IsInstanceOf('Medium'));
+            scale(Medium()    , "dist", 1e3, "time", 1e6, "mass", 1e6);
+            scale(Scatterers(), "dist", 1e3, "time", 1e6);
+
         end
         function staticTest(test)
             % static constructors
